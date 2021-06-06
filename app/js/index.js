933644b0470659b963ad2bac8d418755cc3e87e1
Roman Vinichenko <chicagezzz@gmail.com>
2021-06-07 02:18:30 +0300

    full desktop

--- app/js/index.js
+++ app/js/index.js
@@ -3,5 +3,14 @@ $(function () {
     dots: true,
     slidesToShow: 3,
     slidesToScroll: 1,
+    swipeToSlide: true,
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.header').addClass('header--active');
+    } else {
+      $('.header').removeClass('header--active');
+    }
   });
 });
