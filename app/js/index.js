8cd3a00c4a1b7b782f3c40cd3d0b22f84f39a5b3
Roman Vinichenko <chicagezzz@gmail.com>
2021-06-08 02:17:57 +0300

    full tasks

--- app/js/index.js
+++ app/js/index.js
@@ -41,4 +41,38 @@ $(function () {
   $('.menu__item-link, .header__logo').on('click', function () {
     $('.menu__items--active').removeClass('menu__items--active');
   });
+
+  $('.intro__content-btn').on('click', function () {
+    console.log('Moving to choice to best products');
+  });
+  $('.products__btn').on('click', function () {
+    console.log('Show me more products please');
+  });
+  $('.footer__item-btn').on('click', function () {
+    console.log('Notify me about updates');
+  });
+  $('.products__nav-btn').on('click', function () {
+    console.log('Add to my cart');
+  });
+});
+
+$(document).ready(function () {
+  var list = $('.products__items li');
+  var numToShow = 8;
+  var button = $('.products__btn');
+  var numInList = list.length;
+  list.hide();
+  if (numInList > numToShow) {
+    button.show();
+  }
+  list.slice(0, numToShow).show();
+
+  button.click(function () {
+    var showing = list.filter(':visible').length;
+    list.slice(showing - 1, showing + numToShow).fadeIn();
+    var nowShowing = list.filter(':visible').length;
+    if (nowShowing >= numInList) {
+      button.hide();
+    }
+  });
 });
