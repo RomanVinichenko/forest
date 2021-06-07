4674458b509a28e19a9ed6c9d79d758eb59d6ce7
Roman Vinichenko <chicagezzz@gmail.com>
2021-06-08 01:21:51 +0300

    full project

--- app/js/index.js
+++ app/js/index.js
@@ -1,16 +1,44 @@
 $(function () {
+  $('.menu a, footer a').on('click', function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate(
+      {
+        scrollTop: top,
+      },
+      1200,
+    );
+  });
+
   $('.articles__items').slick({
     dots: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     swipeToSlide: true,
+    autoPlay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 1270,
+        settings: 'unslick',
+      },
+    ],
   });
 
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
+    if ($(this).scrollTop() > 10) {
       $('.header').addClass('header--active');
     } else {
       $('.header').removeClass('header--active');
     }
+  });
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__items').toggleClass('menu__items--active');
+  });
+
+  $('.menu__item-link, .header__logo').on('click', function () {
+    $('.menu__items--active').removeClass('menu__items--active');
   });
 });
